71b98929c54531b066345af1eb16612118604d51
Feline Huhn <feline.huhn@gmx.de>
2024-01-16 08:53:34 +0100

    add solution 1 for toggle answer button logic

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+// Bookmark button ---------------------------------------------------------
+
 const bookmarkButton = document.querySelector('[data-js="bookmark"]');
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
+});
+
+// Answer button -----------------------------------------------------------
+
+const showAnswerButton = document.querySelector('[data-js="button-answer"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+// Solution 1
+
+let isAnswerDisplayed = false;
+
+showAnswerButton.addEventListener("click", () => {
+  answer.classList.toggle("card__answer--active");
+
+  isAnswerDisplayed = !isAnswerDisplayed;
+  showAnswerButton.textContent = isAnswerDisplayed
+    ? "Hide answer"
+    : "Show answer";
 });
